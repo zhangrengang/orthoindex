c4d29b1b5cf77e11c2dac35398f22acec1fbb59b
zhangrengang <zhangrenang@ori-gene.cn>
2026-08-14 11:34:51 +0800

    Revert "depth: fix: tip alignment dashed lines use Phylo layout y (n-i), matching tip rows"
    This reverts commit aa638d06a2beb3ceb288053dcadf418a3c4feb24.

--- soi/ploidy_plotter.py
+++ soi/ploidy_plotter.py
@@ -280,11 +280,9 @@ def _draw_cladogram(ax, sptree, sps):
 		tip_x[t] = x
 	x_end = max(tip_x.values())
 	ax.set_xlim(ax.get_xlim()[0], x_end)
-	n = len(tips)
 	for i, t in enumerate(tips):
-		# Phylo layout y = n - i (tip 0 at y=n, top after y-invert);
-		# draw dashed alignment line at that y
-		ax.plot([tip_x[t], x_end], [n - i, n - i],
+		# Phylo.draw y is 1-based (tip 0 at y=1, top since ylim inverted)
+		ax.plot([tip_x[t], x_end], [i + 1, i + 1],
 				color='0.6', lw=0.6, ls=':', zorder=0)
 	ax.set_xticks([])
 	ax.set_yticks([])
